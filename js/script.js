17555bdb3f035a277d18778fe7062697e3639313
Oskar Grzybwski <oskargrzybowski50@gmail.com>
2021-01-13 13:46:49 +0100

    Poprawiony kalkulator

--- js/script.js
+++ js/script.js
@@ -2,19 +2,21 @@ function bmi(){
     let wzrost = document.querySelector(".metry").value;
     let waga = document.querySelector(".waga").value;
     let bmi = waga / Math.pow(wzrost, 2);
+     
+ 
     if(bmi < 18.5){
-        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+ "Masz niedowage"
+        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+ bmi.toFixed(2) +"<br>"+ "Masz niedowage"
     }
    else if (bmi >= 18.5 && bmi <= 24.99){
-        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+ "Twoja waga jest prawidłowa"
+        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+ bmi.toFixed(2) +"<br>"+ "Twoja waga jest prawidłowa"
     } else if (bmi >= 25.0 && bmi <=29.99){
-        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+ "Masz nadwage"
+        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+ bmi.toFixed(2) +"<br>"+ "Masz nadwage"
     } else if (bmi >= 30.0 && bmi <=34.99){
-        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+ "Masz otyłość 1 stopnia"
+        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+ bmi.toFixed(2) +"<br>"+ "Masz otyłość 1 stopnia"
     } else if (bmi >= 35.0 && bmi <=39.99){
-        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+ "Masz otyłość 2 stopnia"
+        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+ bmi.toFixed(2) +"<br>"+ "Masz otyłość 2 stopnia"
     } else if (bmi >= 40){
-        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+ "Masz otyłość 3 stopnia"
+        document.querySelector("p").innerHTML = "Twój wskaźnik BMI wynosi: "+ bmi.toFixed(2) +"<br>"+ "Masz otyłość 3 stopnia"
     }
 }
 
